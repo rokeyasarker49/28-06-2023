6f7d4988a1d837a16589067d1ded2ccd7dbc5c2d
Rokeya Sarker <133252712+rokeyasarker49@users.noreply.github.com>
2023-06-29 00:38:27 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+//responsive bottom navbar
+
+function myFunction() {
+        var x = document.getElementById("myNavbar");
+        if (x.className === "navbar") {
+          x.className += " responsive";
+        } else {
+          x.className = "navbar";
+        }
+}
